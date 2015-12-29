03378fd9fda0f4a452169ec0e36065e60ec1e13e
probonopd <probonopd@users.noreply.github.com>
2015-12-30 00:15:59 +0100

    Only use sudo if we are not already root
    This can be useful when running as root and the sudo command is not installed [ci skip]

--- build.sh
+++ build.sh
@@ -7,15 +7,20 @@
 
 HERE="$(dirname "$(readlink -f "${0}")")"
 
+# Figure out whether we should use sudo
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
+
 if [ -e /usr/bin/apt-get ] ; then
-  sudo apt-get update
-  sudo apt-get -y install libfuse-dev libglib2.0-dev cmake git libc6-dev binutils fuse
+  $SUDO apt-get update
+  $SUDO apt-get -y install libfuse-dev libglib2.0-dev cmake git libc6-dev binutils fuse
 
 fi
 
 if [ -e /usr/bin/yum ] ; then
-  sudo yum -y install git cmake binutils fuse glibc-devel glib2-devel fuse-devel gcc zlib-devel libpng12
-
+  $SUDO yum -y install git cmake binutils fuse glibc-devel glib2-devel fuse-devel gcc zlib-devel libpng12
 fi
 
 cd "${HERE}"
