8750a98748d5ffb2a9be3d1431385fab403dcddf
probonopd <probonopd@users.noreply.github.com>
2016-06-04 15:14:44 +0200

    Update package.sh

--- sandbox/package.sh
+++ sandbox/package.sh
@@ -10,31 +10,15 @@ OUTPUT_FILENAME="${PROGRAMNAME}_${VERSION}_${ARCH}.deb"
 echo "creating deb for '${PROGRAMNAME}'; output '${OUTPUT_FILENAME}'"
 
 mkdir -v -p debian/DEBIAN
-mkdir -v -p debian/usr/bin
-
-DESKTOPFILESDIR="debian/usr/share/applications"
-chmod 644 "${DESKTOPFILESDIR}/${PROGRAMNAME}.desktop"
-
-DOCSDIR="debian/usr/share/doc/${PROGRAMNAME}"
-
-ICONDIR="debian/usr/share/icons/hicolor/scalable/apps"
-ICONPATH="${ICONDIR}/${PROGRAMNAME}.svg"
-chmod 644 "${ICONPATH}"
-
-find debian -type d | xargs chmod 755
-
-# mv -v "${PROGRAMNAME}/${PROGRAMNAME}" debian/usr/bin/
-chmod 755 debian/usr/bin/*
+cp -rf ./src/* ./debian/
 
 cat "License to be determined" > "${DOCSDIR}/copyright"
-find "${DOCSDIR}" -type f -exec chmod 0644 {} \;
 
 mkdir -p debian/usr/share/doc/${PROGRAMNAME}/
 
 find debian/usr -type d -exec chmod 0755 {} \;
 
 echo "" | gzip -9 - -c -f > debian/usr/share/doc/${PROGRAMNAME}/changelog.gz
-chmod 0644 debian/usr/share/doc/${PROGRAMNAME}/changelog.gz
 
 # round((size in bytes)/1024)
 INSTALLED_SIZE=$(du -s debian/usr | awk '{x=$1/1024; i=int(x); if ((x-i)*10 >= 5) {f=1} else {f=0}; print i+f}')
@@ -50,6 +34,10 @@ Maintainer: Simon Peter <probono@puredarwin.org>
 Description: AppImage Sandbox 
  Registers AppImage file format with a helper that runs
  them inside a sandbox with read-only filesystem access." > debian/DEBIAN/control
+
+find ./debian/ -type d | xargs chmod 755
+find ./debian/ -type f -exec chmod 0644 {} \;
+chmod 755 debian/usr/bin/*
 
 fakeroot dpkg-deb --build debian
 mv -v debian.deb "${OUTPUT_FILENAME}"
