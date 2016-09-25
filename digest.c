f9b7ea46be450cbb9867a311fc66df127540316b
probonopd <probonopd@users.noreply.github.com>
2016-09-25 10:55:08 +0200

    Fix digest algorithm

--- digest.c
+++ digest.c
@@ -11,21 +11,6 @@
 
 typedef unsigned char byte;
 
-void sha256_string(char *string, char outputBuffer[65])
-{
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, string, strlen(string));
-    SHA256_Final(hash, &sha256);
-    int i = 0;
-    for(i = 0; i < SHA256_DIGEST_LENGTH; i++)
-    {
-        sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
-    }
-    outputBuffer[64] = 0;
-}
-
 int sha256_file(char *path, char outputBuffer[65], int skip_offset, int skip_length)
 {
     FILE *file = fopen(path, "rb");
@@ -83,8 +68,16 @@ int sha256_file(char *path, char outputBuffer[65], int skip_offset, int skip_len
         SHA256_Update(&sha256, buffer, bytesRead);
     }
     SHA256_Final(hash, &sha256);
-    sha256_string(hash, outputBuffer);
+
     printf("totalBytesRead: %i\n", totalBytesRead);
+    
+    int i = 0;
+    for(i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
+    }
+    outputBuffer[64] = 0;
+    
     return 0;
 }
 
