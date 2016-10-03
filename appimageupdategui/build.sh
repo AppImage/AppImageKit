d1a8d10b0c2d0dbe6524cb102db20ce6cb299dc0
probonopd <probonopd@users.noreply.github.com>
2016-10-03 21:13:31 +0200

    $(which yum)
    [ci skip]

--- appimageupdategui/build.sh
+++ appimageupdategui/build.sh
@@ -7,7 +7,7 @@ if [ "$(which apt-get)" != "" ] ; then
   sudo apt-get -y install libgtk-3-dev valac-0.30 clang cmake libvala-0.30 git
 fi
 
-if [ "(which yum)" != "" ] ; then
+if [ "$(which yum)" != "" ] ; then
   sudo yum -y install gtk3-devel vala clang cmake vala-devel
 fi
 
