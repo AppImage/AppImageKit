0a3cfea2452d1a4482ca57d869c424e85b3792ef
Bignaux Ronan <ronan@aimao.org>
2016-10-18 06:05:47 +0200

    fix liblz4.pc location

--- install-build-deps.sh
+++ install-build-deps.sh
@@ -6,5 +6,5 @@ if [ -e /usr/bin/apt-get ] ; then
   sudo apt-get -y install git autoconf libtool make gcc libtool libfuse-dev \
   liblzma-dev libglib2.0-dev libssl-dev libinotifytools0-dev liblz4-dev
   # libtool-bin might be required in newer distributions but is not available in precise
-  sudo cp pkgconfig/liblz4.pc /usr/lib/x86_64-linux-gnu/pkgconfig/
+  sudo cp resource/liblz4.pc /usr/lib/x86_64-linux-gnu/pkgconfig/
 fi
