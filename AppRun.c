26c04f7bcd7630f298366e6f4897a1fc239e6e1f
Sod-Almighty <sod.almighty@gmail.com>
2017-04-09 07:53:44 +0100

    Added support for Exec= arguments in .desktop file (#377)
    Added support for Exec= arguments in .desktop file

--- AppRun.c
+++ AppRun.c
@@ -32,31 +32,31 @@ THE SOFTWARE.
 #include <dirent.h>
 #include <string.h>
 
-#define die(...)                                \
-    do {                                        \
-        fprintf(stderr, "Error: " __VA_ARGS__);   \
-        exit(1);                              \
-    } while (0);
+#define die(...)                                    \
+    do {                                            \
+        fprintf(stderr, "Error: " __VA_ARGS__);     \
+        exit(1);                                    \
+    } while(0);
+#define MAX(a,b)    (a > b ? a : b)
+#define bool int
+#define false 0
+#define true -1
 
 #define LINE_SIZE 255
 
-int filter (const struct dirent *dir)
-{
+int filter(const struct dirent *dir) {
     char *p = (char*) &dir->d_name;
     p = strrchr(p, '.');
     return p && !strcmp(p, ".desktop");
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     char *appdir = dirname(realpath("/proc/self/exe", NULL));
     if (!appdir)
         die("Could not access /proc/self/exe\n");
-    
+
     int ret;
-
     struct dirent **namelist;
-
     ret = scandir(appdir, &namelist, filter, NULL);
 
     if (ret == 0) {
@@ -64,94 +64,133 @@ int main(int argc, char *argv[])
     } else if(ret == -1) {
         die("Could not scan directory %s\n", appdir);
     }
-    
-    /* Extract executable from .desktop file */
 
-    FILE *f;
+    /* Extract executable from .desktop file */
     char *desktop_file = malloc(LINE_SIZE);
     snprintf(desktop_file, LINE_SIZE-1, "%s/%s", appdir, namelist[0]->d_name);
-    f = fopen(desktop_file, "r");
+    FILE *f     = fopen(desktop_file, "r");
+    char *line  = malloc(LINE_SIZE);
+    char *exe   = line+5;
+    size_t n    = LINE_SIZE;
 
-    char *line = malloc(LINE_SIZE);
-    size_t n = LINE_SIZE;
-    int found = 0;
-
-    while (getline(&line, &n, f) != -1)
-    {
-        if (!strncmp(line,"Exec=",5))
-        {
-            char *p = line+5;
-            while (*++p && *p != ' ' &&  *p != '%'  &&  *p != '\n');
-            *p = 0;
-            found = 1;
-            break;
-        }
-    }
-
+    do {
+        if (getline(&line, &n, f) == -1)
+            die("Executable not found, make sure there is a line starting with 'Exec='\n");
+    } while(strncmp(line, "Exec=", 5));
     fclose(f);
 
-    if (!found)
-      die("Executable not found, make sure there is a line starting with 'Exec='\n");
+    // parse arguments
+    bool in_quotes = 0;
+    for (n = 0; n < LINE_SIZE; n++) {
+        if (!line[n])         // end of string
+            break;
+        else if (line[n] == 10 || line[n] == 13) {
+            line[n] = '\0';
+            line[n+1] = '\0';
+            line[n+2] = '\0';
+            break;
+        } else if (line[n] == '"') {
+            in_quotes = !in_quotes;
+        } else if (line[n] == ' ' && !in_quotes)
+            line[n] = '\0';
+    }
 
-    /* Execution */
-    char *executable = basename(line+5);
+    // count arguments
+    char*   arg         = exe;
+    int     argcount    = 0;
+    while ((arg += (strlen(arg)+1)) && *arg)
+        argcount += 1;
 
+    // merge args
+    char*   outargptrs[argcount + argc + 1];
+    outargptrs[0] = exe;
+    int     outargindex = 1;
+    arg                 = exe;
+    int     argc_       = argc - 1;     // argv[0] is the filename
+    char**  argv_       = argv + 1;
+    while ((arg += (strlen(arg)+1)) && *arg) {
+        if (arg[0] == '%' || (arg[0] == '"' && arg[1] == '%')) {         // handle desktop file field codes
+            char code = arg[arg[0] == '%' ? 1 : 2];
+            switch(code) {
+                case 'f':
+                case 'u':
+                    if (argc_ > 0) {
+                        outargptrs[outargindex++] = *argv_++;
+                        argc_--;
+                    }
+                    break;
+                case 'F':
+                case 'U':
+                    while (argc_ > 0) {
+                        outargptrs[outargindex++] = *argv_++;
+                        argc_--;
+                    }
+                    break;
+                case 'i':
+                case 'c':
+                case 'k':
+                    fprintf(stderr, "WARNING: Desktop file field code %%%c is not currently supported\n", code);
+                    break;
+                default:
+                    fprintf(stderr, "WARNING: Invalid desktop file field code %%%c\n", code);
+                    break;
+            }
+        } else {
+            outargptrs[outargindex++] = arg;
+        }
+    }
+    while (argc_ > 0) {
+        outargptrs[outargindex++] = *argv_++;
+        argc_--;
+    }
+    outargptrs[outargindex] = '\0';     // trailing null argument required by execvp()
+
+    // change directory
     char usr_in_appdir[1024];
     sprintf(usr_in_appdir, "%s/usr", appdir);
     ret = chdir(usr_in_appdir);
     if (ret != 0)
         die("Could not cd into %s\n", usr_in_appdir);
 
-    /* Build environment */
+    // set environment variables
     char *old_env;
-    int length = 2047;
-    char new_env1[length+1];
-    char new_env2[length+1];
-    char new_env3[length+1];
-    char new_env4[length+1];
-    char new_env5[length+1];
-    char new_env6[length+1];
-    char new_env7[length+1];
-    char new_envp[length+1];
-    
-    old_env = getenv("PATH") ?: "";
-    snprintf(new_env1, length, "PATH=%s/usr/bin/:%s/usr/sbin/:%s/usr/games/:%s/bin/:%s/sbin/:%s", appdir, appdir, appdir, appdir, appdir, old_env);
-    putenv(new_env1);
-    
-    old_env = getenv("LD_LIBRARY_PATH") ?: "";
-    snprintf(new_env2, length, "LD_LIBRARY_PATH=%s/usr/lib/:%s/usr/lib/i386-linux-gnu/:%s/usr/lib/x86_64-linux-gnu/:%s/usr/lib32/:%s/usr/lib64/:%s/lib/:%s/lib/i386-linux-gnu/:%s/lib/x86_64-linux-gnu/:%s/lib32/:%s/lib64/:%s", appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, old_env);
-    putenv(new_env2);
-
-    old_env = getenv("PYTHONPATH") ?: "";
-    snprintf(new_env3, length, "PYTHONPATH=%s/usr/share/pyshared/:%s", appdir, old_env);
-    putenv(new_env3);
+    const LENGTH = 2047;
+    char new_env[8][LENGTH+1];
 
     /* https://docs.python.org/2/using/cmdline.html#envvar-PYTHONHOME */
-    snprintf(new_envp, length, "PYTHONHOME=%s/usr/", appdir);
-    putenv(new_envp);
-    
+    snprintf(new_env[0], LENGTH, "PYTHONHOME=%s/usr/", appdir);
+
+    int a,b;
+    old_env = getenv("PATH") ?: "";
+    snprintf(new_env[1], LENGTH, "PATH=%s/usr/bin/:%s/usr/sbin/:%s/usr/games/:%s/bin/:%s/sbin/:%s", appdir, appdir, appdir, appdir, appdir, old_env);
+
+    old_env = getenv("LD_LIBRARY_PATH") ?: "";
+    snprintf(new_env[2], LENGTH, "LD_LIBRARY_PATH=%s/usr/lib/:%s/usr/lib/i386-linux-gnu/:%s/usr/lib/x86_64-linux-gnu/:%s/usr/lib32/:%s/usr/lib64/:%s/lib/:%s/lib/i386-linux-gnu/:%s/lib/x86_64-linux-gnu/:%s/lib32/:%s/lib64/:%s", appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, old_env);
+
+    old_env = getenv("PYTHONPATH") ?: "";
+    snprintf(new_env[3], LENGTH, "PYTHONPATH=%s/usr/share/pyshared/:%s", appdir, old_env);
+
     old_env = getenv("XDG_DATA_DIRS") ?: "";
-    snprintf(new_env4, length, "XDG_DATA_DIRS=%s/usr/share/:%s", appdir, old_env);
-    putenv(new_env4);
+    snprintf(new_env[4], LENGTH, "XDG_DATA_DIRS=%s/usr/share/:%s", appdir, old_env);
 
     old_env = getenv("PERLLIB") ?: "";
-    snprintf(new_env5, length, "PERLLIB=%s/usr/share/perl5/:%s/usr/lib/perl5/:%s", appdir, appdir, old_env);
-    putenv(new_env5);
+    snprintf(new_env[5], LENGTH, "PERLLIB=%s/usr/share/perl5/:%s/usr/lib/perl5/:%s", appdir, appdir, old_env);
 
     /* http://askubuntu.com/questions/251712/how-can-i-install-a-gsettings-schema-without-root-privileges */
     old_env = getenv("GSETTINGS_SCHEMA_DIR") ?: "";
-    snprintf(new_env6, length, "GSETTINGS_SCHEMA_DIR=%s/usr/share/glib-2.0/schemas/:%s", appdir, old_env);
-    putenv(new_env6);
-    
+    snprintf(new_env[6], LENGTH, "GSETTINGS_SCHEMA_DIR=%s/usr/share/glib-2.0/schemas/:%s", appdir, old_env);
+
     old_env = getenv("QT_PLUGIN_PATH") ?: "";
-    snprintf(new_env7, length, "QT_PLUGIN_PATH=%s/usr/lib/qt4/plugins/:%s/usr/lib/i386-linux-gnu/qt4/plugins/:%s/usr/lib/x86_64-linux-gnu/qt4/plugins/:%s/usr/lib32/qt4/plugins/:%s/usr/lib64/qt4/plugins/:%s/usr/lib/qt5/plugins/:%s/usr/lib/i386-linux-gnu/qt5/plugins/:%s/usr/lib/x86_64-linux-gnu/qt5/plugins/:%s/usr/lib32/qt5/plugins/:%s/usr/lib64/qt5/plugins/:%s", appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, old_env);
-    putenv(new_env7);
-    
+    snprintf(new_env[7], LENGTH, "QT_PLUGIN_PATH=%s/usr/lib/qt4/plugins/:%s/usr/lib/i386-linux-gnu/qt4/plugins/:%s/usr/lib/x86_64-linux-gnu/qt4/plugins/:%s/usr/lib32/qt4/plugins/:%s/usr/lib64/qt4/plugins/:%s/usr/lib/qt5/plugins/:%s/usr/lib/i386-linux-gnu/qt5/plugins/:%s/usr/lib/x86_64-linux-gnu/qt5/plugins/:%s/usr/lib32/qt5/plugins/:%s/usr/lib64/qt5/plugins/:%s", appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, old_env);
+
+    for (n = 0; n < 8; n++)
+        putenv(new_env[n]);
+
     /* Run */
-    ret = execvp(executable, argv); // FIXME: What about arguments in the Exec= line of the desktop file?
+    ret = execvp(exe, outargptrs);
 
     if (ret == -1)
-        die("Error executing '%s'; return code: %d\n", executable, ret);
+        die("Error executing '%s'; return code: %d\n", exe, ret);
 
     free(line);
     free(desktop_file);
