b053bf5d02a248307610e110ef9a4d0957d754b2
probonopd <probonopd@users.noreply.github.com>
2017-06-03 16:10:29 +0200

    Move to https://github.com/AppImage/AppImageKit/

--- squashfuse_dlopen.c
+++ squashfuse_dlopen.c
@@ -6,6 +6,6 @@ const char *load_library_errmsg =
   "AppImages require FUSE to run. \n"
   "You might still be able to extract the contents of this AppImage \n"
   "if you run it with the --appimage-extract option. \n"
-  "See https://github.com/probonopd/AppImageKit/wiki/FUSE \n"
+  "See https://github.com/AppImage/AppImageKit/wiki/FUSE \n"
   "for more information\n";
 
