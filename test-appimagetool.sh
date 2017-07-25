4d51c3a82c872b5dc559e99b52f485d7749f76aa
TheAssassin <theassassin@users.noreply.github.com>
2017-07-26 00:30:08 +0200

    Improve test script

--- test-appimagetool.sh
+++ test-appimagetool.sh
@@ -22,15 +22,18 @@ trap cleanup EXIT
 cd "$tempdir"
 
 # create a sample AppDir
-mkdir appimagetool.AppDir
-cp -R "$thisdir"/resources/{appimagetool.*,AppRun} appimagetool.AppDir/
-cp -R "$appimagetool" appimagetool.AppDir/
+mkdir -p appimagetool.AppDir/usr/share/metainfo/
+cp "$thisdir"/resources/{appimagetool.*,AppRun} appimagetool.AppDir/
+cp "$thisdir"/resources/usr/share/metainfo/appimagetool.appdata.xml appimagetool.AppDir/usr/share/metainfo/
+cp "$appimagetool" appimagetool.AppDir/
+mkdir -p appimagetool.AppDir/usr/share/applications
+cp appimagetool.AppDir/appimagetool.desktop appimagetool.AppDir/usr/share/applications
 
 # create a file that should be ignored
 touch appimagetool.AppDir/to-be-ignored
 
 # create an AppImage without an ignore file to make sure it is bundled
-$appimagetool appimagetool.AppDir appimagetool.AppImage
+$appimagetool appimagetool.AppDir appimagetool.AppImage || exit $?
 $appimagetool -l appimagetool.AppImage | grep -q to-be-ignored || exit 1
 
 # now set up the ignore file, and check that the file is properly ignored
@@ -39,11 +42,11 @@ $appimagetool appimagetool.AppDir appimagetool.AppImage
 $appimagetool -l appimagetool.AppImage | grep -q to-be-ignored && exit 1 || true
 
 # check if disabling the ignore file feature works by passing an empty string as argument
-$appimagetool appimagetool.AppDir appimagetool.AppImage --exclude-file ""
+$appimagetool appimagetool.AppDir appimagetool.AppImage --exclude-file "" || exit $?
 $appimagetool -l appimagetool.AppImage | grep -q to-be-ignored || exit 1
 
 # remove the default ignore file, and check if an explicitly passed one works, too
 rm .appimageignore
 echo "to-be-ignored" > ignore
-$appimagetool appimagetool.AppDir appimagetool.AppImage --exclude-file ignore
+$appimagetool appimagetool.AppDir appimagetool.AppImage --exclude-file ignore || exit $?
 $appimagetool -l appimagetool.AppImage | grep -q to-be-ignored && exit 1 || true
