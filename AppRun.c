3f62776be7ec9b0ae1fe75d7c8b478b6e9ef5b21
darealshinji <djcj@gmx.de>
2017-10-20 21:57:33 +0200

    use calloc() instead of malloc()

--- AppRun.c
+++ AppRun.c
@@ -40,7 +40,7 @@ THE SOFTWARE.
 #define SET_NEW_ENV(str,len,fmt,...)                \
     format = fmt;                                   \
     length = strlen(format) + (len);                \
-    char *str = malloc(length);                     \
+    char *str = calloc(length);                     \
     snprintf(str, length-1, format, __VA_ARGS__);   \
     putenv(str);
 #define MAX(a,b)    (a > b ? a : b)
