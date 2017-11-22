87e2bb365ab219f8b89f95d5b1c205e587f535d9
Alexis López Zubieta <azubieta90@gmail.com>
2017-11-22 17:04:36 -0600

    Improving appimage lib cmake module files and overall structure.

---
+++ libappimage/cmake/cmake_import_manual_targets.cmake
@@ -0,0 +1,19 @@
+###
+# Registering external targets
+###
+add_library(squashfuse STATIC IMPORTED)
+set_property(TARGET squashfuse PROPERTY IMPORTED_LOCATION "${CMAKE_SOURCE_DIR}/../squashfuse/.libs/libsquashfuse.a")
+
+add_library(fuseprivate STATIC IMPORTED)
+set_property(TARGET fuseprivate PROPERTY IMPORTED_LOCATION "${CMAKE_SOURCE_DIR}/../squashfuse/.libs/libfuseprivate.a")
+
+
+if( ${STATIC_BUILD} )
+    add_library(archive STATIC IMPORTED)
+    set_property(TARGET archive
+        PROPERTY IMPORTED_LOCATION "${CMAKE_SOURCE_DIR}/../libarchive-3.3.1/.libs/libarchive.a")
+
+    add_library(lzma STATIC IMPORTED)
+    set_property(TARGET lzma 
+        PROPERTY IMPORTED_LOCATION "${CMAKE_SOURCE_DIR}/../xz-5.2.3/build/lib/liblzma.a")
+endif( ${STATIC_BUILD} )
--- libappimage/src/appimage.h
+++ libappimage/src/appimage.h
@@ -1,9 +1,6 @@
 #ifndef AppImage
 #define AppImage
 
-
-#include <glib.h>
-
 extern "C" {
     /* Return the md5 hash constructed according to
     * https://specifications.freedesktop.org/thumbnail-spec/thumbnail-spec-latest.html#THUMBSAVE
@@ -11,19 +8,19 @@ extern "C" {
     char *get_md5(char const *path);
 
     /* Check if a file is an AppImage. Returns the image type if it is, or -1 if it isn't */
-    int check_appimage_type( const char *path, gboolean verbose);
+    int check_appimage_type( const char *path, int verbose);
 
     /* Register a type 1 AppImage in the system */
-    bool appimage_type1_register_in_system(const char *path, gboolean verbose);
+    bool appimage_type1_register_in_system(const char *path, int verbose);
 
     /* Register a type 2 AppImage in the system */
-    bool appimage_type2_register_in_system(const char *path, gboolean verbose);
+    bool appimage_type2_register_in_system(const char *path, int verbose);
 
     /* Register an AppImage in the system */
-    int appimage_register_in_system(const char *path, gboolean verbose);
+    int appimage_register_in_system(const char *path, int verbose);
 
     /* Unregister an AppImage in the system */
-    int appimage_unregister_in_system(const char *path, gboolean verbose);
+    int appimage_unregister_in_system(const char *path, int verbose);
 }
 
 #endif // !AppImage
