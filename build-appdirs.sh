4692b7ea8ddb5061a948f46a42fcdcfcb315f9a9
TheAssassin <theassassin@users.noreply.github.com>
2017-12-15 02:58:21 +0100

    Try to place artifacts only in build directory, mk. 3

--- build-appdirs.sh
+++ build-appdirs.sh
@@ -13,17 +13,17 @@ rm -rf "$APPIMAGETOOL_APPDIR" || true
 mkdir -p "$APPIMAGETOOL_APPDIR"/usr/bin
 cp -f install_prefix/usr/bin/appimagetool "$APPIMAGETOOL_APPDIR"/usr/bin
 
-cp resources/AppRun "$APPIMAGETOOL_APPDIR"
+cp ../resources/AppRun "$APPIMAGETOOL_APPDIR"
 cp install_prefix/usr/bin/appimagetool "$APPIMAGETOOL_APPDIR"/usr/bin/
 cp install_prefix/usr/bin/mksquashfs "$APPIMAGETOOL_APPDIR"/usr/bin/
 cp $(which desktop-file-validate) "$APPIMAGETOOL_APPDIR"/usr/bin/
 cp $(which zsyncmake) "$APPIMAGETOOL_APPDIR"/usr/bin/
 
-cp resources/appimagetool.desktop "$APPIMAGETOOL_APPDIR"
-cp resources/appimagetool.svg "$APPIMAGETOOL_APPDIR"/appimagetool.svg
+cp ../resources/appimagetool.desktop "$APPIMAGETOOL_APPDIR"
+cp ../resources/appimagetool.svg "$APPIMAGETOOL_APPDIR"/appimagetool.svg
 ln -s "$APPIMAGETOOL_APPDIR"/appimagetool.svg "$APPIMAGETOOL_APPDIR"/.DirIcon
 mkdir -p "$APPIMAGETOOL_APPDIR"/usr/share/metainfo
-cp resources/usr/share/metainfo/appimagetool.appdata.xml "$APPIMAGETOOL_APPDIR"/usr/share/metainfo/
+cp ../resources/usr/share/metainfo/appimagetool.appdata.xml "$APPIMAGETOOL_APPDIR"/usr/share/metainfo/
 
 #######################################################################
 
@@ -37,11 +37,11 @@ mkdir -p "$APPIMAGED_APPDIR"/usr/lib
 cp -f install_prefix/usr/bin/appimaged "$APPIMAGED_APPDIR"/usr/bin
 cp -f install_prefix/usr/bin/validate "$APPIMAGED_APPDIR"/usr/bin
 mkdir -p "$APPIMAGED_APPDIR"/usr/share/metainfo
-cp resources/usr/share/metainfo/appimaged.appdata.xml "$APPIMAGED_APPDIR"/usr/share/metainfo/
+cp ../resources/usr/share/metainfo/appimaged.appdata.xml "$APPIMAGED_APPDIR"/usr/share/metainfo/
 
-cp resources/AppRun "$APPIMAGED_APPDIR"/
+cp ../resources/AppRun "$APPIMAGED_APPDIR"/
 find /usr -name "libarchive.so.*.*" -exec cp "{}" "$APPIMAGED_APPDIR"/usr/lib/ \; > /dev/null 2>&1
 
-cp resources/appimaged.desktop "$APPIMAGED_APPDIR"/
-cp resources/appimagetool.svg "$APPIMAGED_APPDIR"/appimaged.svg
+cp ../resources/appimaged.desktop "$APPIMAGED_APPDIR"/
+cp ../resources/appimagetool.svg "$APPIMAGED_APPDIR"/appimaged.svg
 ( cd "$APPIMAGED_APPDIR"/ ; ln -s appimaged.svg .DirIcon )
