0c4d21d9ca448e9300ddb4489f6a0f2d909bdbe7
TheAssassin <theassassin@users.noreply.github.com>
2017-12-17 20:15:49 +0100

    Update build deps

--- install-build-deps.sh
+++ install-build-deps.sh
@@ -14,7 +14,7 @@ fi
 if [ -e /usr/bin/zypper ] ; then
     sudo zypper refresh
     sudo zypper in -y build git-core gcc g++ wget make glibc-devel glib2-devel libarchive-devel \
-        fuse fuse-devel zlib-devel patch cairo-devel zsync desktop-file-utils
+        fuse fuse-devel zlib-devel patch cairo-devel zsync desktop-file-utils cmake
     #for some reason openSUSE Tumbleweed have apt-get.
     return
 fi
@@ -22,7 +22,7 @@ fi
 if [ -e /usr/bin/apt-get ] ; then
   sudo apt-get update
   sudo apt-get -y install zsync git libarchive-dev autoconf libtool make gcc g++ libtool libfuse-dev \
-  liblzma-dev libglib2.0-dev libssl-dev libinotifytools0-dev liblz4-dev equivs libcairo-dev desktop-file-utils
+  liblzma-dev libglib2.0-dev libssl-dev libinotifytools0-dev liblz4-dev equivs libcairo-dev desktop-file-utils cmake
   # libtool-bin might be required in newer distributions but is not available in precise
   sudo cp resources/liblz4.pc /usr/lib/$ARCH-linux-gnu/pkgconfig/
 fi
@@ -48,7 +48,7 @@ if [ -e /usr/bin/yum ] ; then
   fi
 
   yum -y install epel-release
-  yum -y install git wget make binutils fuse glibc-devel glib2-devel libarchive3-devel fuse-devel zlib-devel patch openssl-static openssl-devel vim-common cairo-devel desktop-file-utils # inotify-tools-devel lz4-devel
+  yum -y install git wget make binutils fuse glibc-devel glib2-devel libarchive3-devel fuse-devel zlib-devel patch openssl-static openssl-devel vim-common cairo-devel desktop-file-utils cmake # inotify-tools-devel lz4-devel
 
   if [ "$ARCH" == "x86_64" ]; then
     . /opt/rh/devtoolset-4/enable
@@ -59,7 +59,7 @@ fi
 
 # Install dependencies for Arch Linux
 if [ -e /usr/bin/pacman ] ; then
-  echo "Checking arch package provides and installed packages"
+  echo "Checking arch package provides and installed packages" "cmake"
   declare -a arr=("zsync" "git" "libarchive" "autoconf" "libtool" "make"
     "libtool" "fuse2" "xz" "glib2" "openssl" "inotify-tools" "lz4" "gcc" "g++")
   for i in "${arr[@]}"
@@ -73,7 +73,3 @@ if [ -e /usr/bin/pacman ] ; then
       sudo pacman -S --needed $TO_INSTALL
   fi
 fi
-
-# Install latest CMake
-wget -nv https://github.com/TheAssassin/CMake/releases/download/continuous/cmake-continuous-"$ARCH".AppImage -O /usr/bin/cmake
-chmod +x /usr/bin/cmake
