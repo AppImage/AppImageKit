449a1a56c4440da6b536aafc8e0c66f9f7073feb
TheAssassin <theassassin@users.noreply.github.com>
2017-12-21 18:42:39 +0100

    Fix test script, mk. 10

--- travis/travis-build.sh
+++ travis/travis-build.sh
@@ -55,7 +55,7 @@ docker run \
 cd build/
 
 # test AppImages
-sudo apt-get install -y gcc-multilib lib32z1 fuse
+sudo apt-get install -y gcc-multilib lib32z1 libfuse2
 bash -x ../travis/test-appimages.sh
 
 # install more tools
