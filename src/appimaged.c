c54ef1daa2a025de133ce3da10d5725259193fac
TheAssassin <theassassin@users.noreply.github.com>
2017-12-21 14:39:20 +0100

    Fix --no-install

--- src/appimaged.c
+++ src/appimaged.c
@@ -305,7 +305,7 @@ int main(int argc, char ** argv) {
 
     /* When we run from inside an AppImage, then we check if we are installed
      * in a per-user location and if not, we install ourselves there */
-    if(no_install || (appimage_location != NULL && own_desktop_file_location != NULL)) {
+    if(!no_install || (appimage_location != NULL && own_desktop_file_location != NULL)) {
         if ( (! g_file_test ("/usr/bin/appimaged", G_FILE_TEST_EXISTS)) && ((! g_file_test (global_autostart_file, G_FILE_TEST_EXISTS)) || (! g_file_test (destination, G_FILE_TEST_EXISTS))) && (! g_file_test (global_systemd_file, G_FILE_TEST_EXISTS)) && (! g_file_test (installed_appimaged_location, G_FILE_TEST_EXISTS)) && (g_file_test (own_desktop_file_location, G_FILE_TEST_IS_REGULAR))){
             char *title;
             char *body;
