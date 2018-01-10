364bf6c0d835ad8a673f24177292defd5ddc572a
TheAssassin <theassassin@users.noreply.github.com>
2018-01-10 23:26:19 +0100

    Fix Debian package build, mk. 9

--- travis/travis-build.sh
+++ travis/travis-build.sh
@@ -82,7 +82,7 @@ sudo rm -rf out/*.AppDir out/*.AppImage.digest
 # build Debian packages
 sed -i 's|/AppImageKit|'$(dirname "$PWD")'|g' CPack*.cmake
 find _CPack_Packages -type f -exec sed -i 's|/AppImageKit|'$(dirname "$PWD")'|g' \;
-cpack -V || cat _CPack_Packages/Linux/DEB/PreinstallOutput.log
+cpack -V || cat _CPack_Packages/Linux/DEB/PreinstallOutput.log && exit 1
 
 mv *.deb out/
 
