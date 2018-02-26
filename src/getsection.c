7c72f1ddd82228c21efcb0184f6cb5ace127bc44
TheAssassin <theassassin@users.noreply.github.com>
2018-02-26 23:10:02 +0100

    Reformat code

--- src/getsection.c
+++ src/getsection.c
@@ -7,10 +7,10 @@
 #include "light_elf.h"
 
 /* Return the offset, and the length of an ELF section with a given name in a given ELF file */
-int get_elf_section_offset_and_length(const char* fname, const char* section_name, unsigned long *offset, unsigned long *length)
-{
-    uint8_t *data;   
-    int i;  
+int get_elf_section_offset_and_length(const char* fname, const char* section_name, unsigned long* offset,
+                                      unsigned long* length) {
+    uint8_t* data;
+    int i;
     int fd = open(fname, O_RDONLY);
     data = mmap(NULL, lseek(fd, 0, SEEK_END), PROT_READ, MAP_SHARED, fd, 0);
 
@@ -22,39 +22,39 @@ int get_elf_section_offset_and_length(const char* fname, const char* section_nam
     elf = (Elf32_Ehdr *) data;
     shdr = (Elf32_Shdr *) (data + elf->e_shoff);
 #elif __SIZEOF_POINTER__ == 8
-    Elf64_Ehdr *elf;
-    Elf64_Shdr *shdr;
-    elf = (Elf64_Ehdr *) data;
-    shdr = (Elf64_Shdr *) (data + elf->e_shoff);
+    Elf64_Ehdr* elf;
+    Elf64_Shdr* shdr;
+    elf = (Elf64_Ehdr*) data;
+    shdr = (Elf64_Shdr*) (data + elf->e_shoff);
 #else
     #error Platforms other than 32-bit/64-bit are currently not supported!
 #endif
 
-    char *strTab = (char *)(data + shdr[elf->e_shstrndx].sh_offset);
-    for(i = 0; i <  elf->e_shnum; i++) {
-        if(strcmp(&strTab[shdr[i].sh_name], section_name) == 0) {
-        *offset = shdr[i].sh_offset;
-        *length = shdr[i].sh_size;
+    char* strTab = (char*) (data + shdr[elf->e_shstrndx].sh_offset);
+    for (i = 0; i < elf->e_shnum; i++) {
+        if (strcmp(&strTab[shdr[i].sh_name], section_name) == 0) {
+            *offset = shdr[i].sh_offset;
+            *length = shdr[i].sh_size;
         }
     }
     close(fd);
-    return(0);
+    return (0);
 }
 
-void print_hex(char* fname, unsigned long offset, unsigned long length){
-    uint8_t *data;   
+void print_hex(char* fname, unsigned long offset, unsigned long length) {
+    uint8_t* data;
     unsigned long k;
     int fd = open(fname, O_RDONLY);
     data = mmap(NULL, lseek(fd, 0, SEEK_END), PROT_READ, MAP_SHARED, fd, 0);
     close(fd);
     for (k = offset; k < offset + length; k++) {
         printf("%x", data[k]);
-        }   
-        printf("\n");
+    }
+    printf("\n");
 }
 
-void print_binary(char* fname, unsigned long offset, unsigned long length){
-    uint8_t *data;   
+void print_binary(char* fname, unsigned long offset, unsigned long length) {
+    uint8_t* data;
     unsigned long k, endpos;
 
     int fd = open(fname, O_RDONLY);
