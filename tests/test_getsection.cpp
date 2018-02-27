e4850d524b165b73c9806b20d3d4cb4c79883541
TheAssassin <theassassin@users.noreply.github.com>
2018-02-27 22:50:23 +0100

    Test print_binary and print_hex against appimaged's AppImage
    This is necessary as appimaged contains additional sections we use
    (update information and a signature).

--- tests/test_getsection.cpp
+++ tests/test_getsection.cpp
@@ -39,13 +39,24 @@ TEST_F(GetSectionCTest, test_get_elf_section_offset_and_length) {
 
 
 TEST_F(GetSectionCTest, test_print_binary) {
-    std::string appImagePath = std::string(TEST_DATA_DIR) + "/Echo-x86_64.AppImage";
+    std::string appImagePath = std::string(TEST_DATA_DIR) + "/appimaged-x86_64.AppImage";
 
     unsigned long offset, length;
 
     ASSERT_EQ(get_elf_section_offset_and_length(appImagePath.c_str(), ".upd_info", &offset, &length), 0);
 
     print_binary(appImagePath.c_str(), offset, length);
+}
+
+
+TEST_F(GetSectionCTest, test_print_hex) {
+    std::string appImagePath = std::string(TEST_DATA_DIR) + "/appimaged-x86_64.AppImage";
+
+    unsigned long offset, length;
+
+    ASSERT_EQ(get_elf_section_offset_and_length(appImagePath.c_str(), ".sha256_sig", &offset, &length), 0);
+
+    print_hex(appImagePath.c_str(), offset, length);
 }
 
 int main(int argc, char **argv) {
