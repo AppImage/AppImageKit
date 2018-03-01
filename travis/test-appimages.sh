379db30abdaae05218cac77f1bf02bc578d86e7d
Kurt Pfeifle <KurtPfeifle@users.noreply.github.com>
2018-03-01 17:28:38 +0100

    Extended tests
    I'm not sure what could theoretically happen on Travis, if indeed the "timeout" commands won't work. Some time later there be a Travis timeout. But will we then at all be able see what our ">&2" redirections print?
    
    Or isn't this script running on Travis at all? Only meant for manual execution?

--- travis/test-appimages.sh
+++ travis/test-appimages.sh
@@ -14,12 +14,49 @@ error() {
 
 # first of all, try to run appimagetool
 out/appimagetool-"$ARCH".AppImage && error  # should fail due to missing parameter
+out/appimaged-"$ARCH".AppImage && error  # should fail due to missing parameter
 out/appimagetool-"$ARCH".AppImage -h || error  # should not fail
+out/appimaged-"$ARCH".AppImage -h || error  # should not fail
 
-# print version and update information
+# print versions, updateinfo and other info, if present
+# first, appimagetool
 out/appimagetool-"$ARCH".AppImage --version || error  # should not fail
 out/appimagetool-"$ARCH".AppImage --appimage-version || error  # should not fail
 out/appimagetool-"$ARCH".AppImage --appimage-updateinformation || error  # should not fail
+out/appimagetool-"$ARCH".AppImage --appimage-updateinfo || error  # should not fail
+out/appimagetool-"$ARCH".AppImage --appimage-signature || error  # should not fail
+out/appimagetool-"$ARCH".AppImage --appimage-offset || error  # should not fail
+# next, appimaged
+out/appimaged-"$ARCH".AppImage --version || error  # should not fail
+out/appimaged-"$ARCH".AppImage --appimage-version || error  # should not fail
+out/appimaged-"$ARCH".AppImage --appimage-updateinformation || error  # should not fail
+out/appimaged-"$ARCH".AppImage --appimage-updateinfo || error  # should not fail
+out/appimaged-"$ARCH".AppImage --appimage-signature || error  # should not fail
+out/appimaged-"$ARCH".AppImage --appimage-offset || error  # should not fail
+
+# check creation of .home and .config files (even if never used by these tools)
+out/appimagetool-"$ARCH".AppImage --appimage-portable-home || error  # should not fail
+out/appimagetool-"$ARCH".AppImage --appimage-portable-config || error  # should not fail
+out/appimaged-"$ARCH".AppImage --appimage-portable-home || error  # should not fail
+out/appimaged-"$ARCH".AppImage --appimage-portable-config || error  # should not fail
+[ -d out/appimagetool-"$ARCH".AppImage.home ] || error   # should not fail
+[ -d out/appimagetool-"$ARCH".AppImage.config ] || error   # should not fail
+[ -d out/appimaged-"$ARCH".AppImage.home ] || error   # should not fail
+[ -d out/appimaged-"$ARCH".AppImage.config ] || error   # should not fail
+# Don't care cleaning up out/appimage{d,tool}-"$ARCH".AppImage.{config,home}... Or should we?
+
+# now test --appimage-mount
+timeout "$TIMEOUT" out/appimaged-"$ARCH".AppImage    --appimage-mount ; ret_of_appimaged_mount="$?"
+timeout "$TIMEOUT" out/appimagetool-"$ARCH".AppImage --appimage-mount ; ret_of_appimagetool_mount="$?"
+echo ret_of_appimaged_mount=$ret_of_appimaged_mount
+echo ret_of_appimagetool_mount=$ret_of_appimagetool_mount
+
+if [[ x$ret_of_appimaged_mount != x124 ]] || [[ x$ret_of_appimagetool_mount != x124 ]] ; then
+    echo "Error: mounting of appimaged or appimagetool did not terminate by timeout as expected" >&2
+    echo "Error: return code of appimaged mount: $ret_of_appimaged_mount"       >&2
+    echo "Error: return code of appimagetool mount: $ret_of_appimagetool_mount" >&2
+    exit 1
+fi
 
 # now check appimaged
 timeout "$TIMEOUT" out/appimaged-"$ARCH".AppImage --no-install
