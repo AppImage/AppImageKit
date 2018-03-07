fb6342d26b65bfb951af0065700c966e8dfaf7c4
TheAssassin <theassassin@users.noreply.github.com>
2018-03-07 15:32:54 +0100

    Introduce new --sign-key argument
    A separate argument makes it easier to be able to specify a key for
    signing the key and exporting the key afterwards. As --sign-args may
    contain additional arguments which would not work when using
    gpg[2] --export, introducing a separate key argument that is then
    passed to both these gpg[2] calls makes more sense.

--- src/appimagetool.c
+++ src/appimagetool.c
@@ -87,7 +87,7 @@ gchar *sqfs_comp = "gzip";
 gchar *exclude_file = NULL;
 gchar *runtime_file = NULL;
 gchar *sign_args = NULL;
-gchar *gpg_key = NULL;
+gchar *sign_key = NULL;
 gchar *pathToMksquashfs = NULL;
 
 // #####################################################################
@@ -439,6 +439,7 @@ static GOptionEntry entries[] =
     { "no-appstream", 'n', 0, G_OPTION_ARG_NONE, &no_appstream, "Do not check AppStream metadata", NULL },
     { "exclude-file", 0, 0, G_OPTION_ARG_STRING, &exclude_file, _exclude_file_desc, NULL },
     { "runtime-file", 0, 0, G_OPTION_ARG_STRING, &runtime_file, "Runtime file to use", NULL },
+    { "sign-key", 0, 0, G_OPTION_ARG_STRING, &sign_key, "Key ID to use for gpg[2] signatures", NULL},
     { "sign-args", 0, 0, G_OPTION_ARG_STRING, &sign_args, "Extra arguments to use when signing with gpg[2]", NULL},
     { G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_FILENAME_ARRAY, &remaining_args, NULL, NULL },
     { 0,0,0,0,0,0,0 }
@@ -917,7 +918,10 @@ main (int argc, char *argv[])
                 if (g_file_test(ascfile, G_FILE_TEST_IS_REGULAR))
                     unlink(ascfile);
 
-                sprintf(command, "%s --detach-sign --armor %s %s", gpg2_path, sign_args ? sign_args : "", digestfile);
+                sprintf(command,
+                    "%s --detach-sign --armor %s %s %s",
+                    gpg2_path, sign_key ? sign_key : "", sign_args ? sign_args : "", digestfile
+                );
 
                 if (verbose)
                     fprintf(stderr, "%s\n", command);
