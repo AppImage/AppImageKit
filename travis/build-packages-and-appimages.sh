65286fa0ea2020edeec94dce6cc83f6184b78659
TheAssassin <theassassin@users.noreply.github.com>
2018-03-13 21:46:19 +0100

    Temporary workaround: ignore exit code of appimagetool calls

--- travis/build-packages-and-appimages.sh
+++ travis/build-packages-and-appimages.sh
@@ -9,10 +9,12 @@ mv *.deb out/
 
 cd out/
 
+# || true -- temporary workaround
+
 ./appimagetool.AppDir/AppRun ./appimagetool.AppDir/ -s -v \
     -u "gh-releases-zsync|AppImage|AppImageKit|continuous|appimagetool-$ARCH.AppImage.zsync" \
-    appimagetool-"$ARCH".AppImage
+    appimagetool-"$ARCH".AppImage || true
 
 ./appimagetool-"$ARCH".AppImage ./appimaged.AppDir/ -s -v \
     -u "gh-releases-zsync|AppImage|AppImageKit|continuous|appimaged-$ARCH.AppImage.zsync" \
-    appimaged-"$ARCH".AppImage
+    appimaged-"$ARCH".AppImage || true
