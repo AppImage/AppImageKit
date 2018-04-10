678cb625974f70d6f72017326d4797dffaea63f5
TheAssassin <theassassin@assassinate-you.net>
2018-04-10 02:20:06 +0200

    Unit tests for X-AppImage-Version, mk. 1

--- tests/fixtures.h
+++ tests/fixtures.h
@@ -21,6 +21,7 @@ public:
 protected:
     std::string appImage_type_1_file_path;
     std::string appImage_type_2_file_path;
+    std::string appImage_type_2_versioned_path;
     std::string appImage_type_2_terminal_file_path;
 
 public:
@@ -56,6 +57,7 @@ public:
 
         appImage_type_1_file_path = std::string(TEST_DATA_DIR) + "/AppImageExtract_6-x86_64.AppImage";
         appImage_type_2_file_path = std::string(TEST_DATA_DIR) + "/Echo-x86_64.AppImage";
+        appImage_type_2_versioned_path = std::string(TEST_DATA_DIR) + "/Echo-test1234-x86_64.AppImage";
         appImage_type_2_terminal_file_path = std::string(TEST_DATA_DIR) + "/appimagetool-x86_64.AppImage";
     };
 
