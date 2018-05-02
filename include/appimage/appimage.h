0f7c6dd51e4ebf8ca45b3ad74b6fd14c96b91573
TheAssassin <theassassin@assassinate-you.net>
2018-05-02 17:17:57 +0200

    Add get_elf_size to libappimage

--- include/appimage/appimage.h
+++ include/appimage/appimage.h
@@ -102,6 +102,22 @@ int appimage_type2_shall_not_be_integrated(const char* path);
  */
 int appimage_shall_not_be_integrated(const char* path);
 
+/*
+ * Calculate the size of an ELF file on disk based on the information in its header
+ *
+ * Example:
+ *
+ * ls -l   126584
+ *
+ * Calculation using the values also reported by readelf -h:
+ * Start of section headers	e_shoff		124728
+ * Size of section headers		e_shentsize	64
+ * Number of section headers	e_shnum		29
+ *
+ * e_shoff + ( e_shentsize * e_shnum ) =	126584
+ */
+ssize_t get_elf_size(const char *fname);
+
 #ifdef __cplusplus
 }
 #endif
