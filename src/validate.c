ffe0b6b92f028930605e3bf387ea7fe0209b852a
TheAssassin <theassassin@assassinate-you.net>
2018-05-09 22:30:02 +0200

    Fix validate build, mk. 2

--- src/validate.c
+++ src/validate.c
@@ -116,7 +116,11 @@ int main(int argc,char **argv)	{
     unsigned long skip_offset = 0;
     unsigned long skip_length = 0;
   
-    get_elf_section_offset_and_length(filename, ".sha256_sig", &skip_offset, &skip_length);
+    if (!appimage_get_elf_section_offset_and_length(filename, ".sha256_sig", &skip_offset, &skip_length)) {
+        fprintf(stderr, "Failed to read .sha256_sig section");
+        exit(1);
+    }
+
     if(skip_length > 0) {
         fprintf(stderr, "Skipping ELF section %s with offset %lu, length %lu\n", segment_name, skip_offset, skip_length);
     } else {
