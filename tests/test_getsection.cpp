00ea7c3c5637c561445790da6114a7e68b174520
TheAssassin <theassassin@assassinate-you.net>
2018-05-11 00:46:20 +0200

    Fix tests build, mk. 3

--- tests/test_getsection.cpp
+++ tests/test_getsection.cpp
@@ -30,7 +30,7 @@ TEST_F(GetSectionCTest, test_appimage_get_elf_section_offset_and_length) {
 
     unsigned long offset, length;
 
-    ASSERT_EQ(appimage_get_elf_section_offset_and_length(appImagePath.c_str(), ".upd_info", &offset, &length), 0);
+    ASSERT_TRUE(appimage_get_elf_section_offset_and_length(appImagePath.c_str(), ".upd_info", &offset, &length));
 
     EXPECT_GT(offset, 0);
     EXPECT_GT(length, 0);
@@ -44,7 +44,7 @@ TEST_F(GetSectionCTest, test_print_binary) {
 
     unsigned long offset, length;
 
-    ASSERT_EQ(appimage_get_elf_section_offset_and_length(appImagePath.c_str(), ".upd_info", &offset, &length), 0);
+    ASSERT_TRUE(appimage_get_elf_section_offset_and_length(appImagePath.c_str(), ".upd_info", &offset, &length));
 
     EXPECT_EQ(print_binary(appImagePath.c_str(), offset, length), 0);
 }
@@ -55,7 +55,7 @@ TEST_F(GetSectionCTest, test_print_hex) {
 
     unsigned long offset, length;
 
-    ASSERT_EQ(appimage_get_elf_section_offset_and_length(appImagePath.c_str(), ".sha256_sig", &offset, &length), 0);
+    ASSERT_TRUE(appimage_get_elf_section_offset_and_length(appImagePath.c_str(), ".sha256_sig", &offset, &length));
 
     EXPECT_EQ(print_hex(appImagePath.c_str(), offset, length), 0);
 }
