b41747ffc04ed28b2de526508a60a759f7aa77ef
TheAssassin <theassassin@assassinate-you.net>
2018-05-14 19:49:15 +0200

    No longer build Debian packages

--- travis/build-appimage.sh
+++ travis/build-appimage.sh
@@ -4,12 +4,7 @@ set -e
 
 cd /AppImageKit/build
 
-cpack -V
-mv *.deb out/
-
 cd out/
-
-# || true -- temporary workaround
 
 ./appimagetool.AppDir/AppRun ./appimagetool.AppDir/ -s -v \
     -u "gh-releases-zsync|AppImage|AppImageKit|continuous|appimagetool-$ARCH.AppImage.zsync" \
