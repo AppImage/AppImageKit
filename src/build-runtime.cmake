a2c6abf6e284c34a4e959e5eb398f550f66a3588
TheAssassin <theassassin@users.noreply.github.com>
2018-07-14 00:41:13 +0200

    Improve documentation

--- src/build-runtime.cmake
+++ src/build-runtime.cmake
@@ -14,27 +14,32 @@ if(APPIMAGEKIT_RUNTIME_ENABLE_SETPROCTITLE)
     set(runtime_cflags "${runtime_cflags} -DENABLE_SETPROCTITLE")
 endif()
 
+# objcopy requires actual files for creating new sections to populate the new section
+# therefore, we generate 3 suitable files containing blank bytes in the right sizes
 add_custom_command(
     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/16_blank_bytes
     COMMAND printf '\\0%.0s' {0..15} > ${CMAKE_CURRENT_BINARY_DIR}/16_blank_bytes
 )
-
 add_custom_command(
     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/1024_blank_bytes
     COMMAND printf '\\0%.0s' {0..1023} > ${CMAKE_CURRENT_BINARY_DIR}/1024_blank_bytes
 )
-
 add_custom_command(
     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/8192_blank_bytes
     COMMAND printf '\\0%.0s' {0..8191} > ${CMAKE_CURRENT_BINARY_DIR}/8192_blank_bytes
 )
 
+# compile first raw object (not linked yet) into which the sections will be embedded
+# TODO: find out how this .o object can be generated using a normal add_executable call
+# that'd allow us to get rid of the -I parameters in runtime_cflags
 add_custom_command(
     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/runtime.0.o
     COMMAND ${CMAKE_C_COMPILER} ${runtime_cflags} -c ${CMAKE_CURRENT_SOURCE_DIR}/runtime.c -o runtime.0.o
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 )
 
+# embed the sections, one by one
+# TODO: find out whether all the sections can be embedded in a single objcopy call
 add_custom_command(
     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/runtime.1.o
     COMMAND objcopy --add-section .digest_md5=16_blank_bytes --set-section-flags .digest_md5=noload,readonly runtime.0.o runtime.1.o
@@ -42,7 +47,6 @@ add_custom_command(
     DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/16_blank_bytes
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 )
-
 add_custom_command(
     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/runtime.2.o
     COMMAND objcopy --add-section .upd_info=1024_blank_bytes --set-section-flags .digest_md5=noload,readonly runtime.1.o runtime.2.o
@@ -50,7 +54,6 @@ add_custom_command(
     DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/1024_blank_bytes
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 )
-
 add_custom_command(
     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/runtime.3.o
     COMMAND objcopy --add-section .sha256_sig=1024_blank_bytes --set-section-flags .digest_md5=noload,readonly runtime.2.o runtime.3.o
@@ -58,7 +61,6 @@ add_custom_command(
     DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/1024_blank_bytes
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 )
-
 add_custom_command(
     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/runtime.4.o
     COMMAND objcopy --add-section .sig_key=8192_blank_bytes --set-section-flags .digest_md5=noload,readonly runtime.3.o runtime.4.o
@@ -67,16 +69,23 @@ add_custom_command(
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 )
 
+# add the runtime as a normal executable
+# CLion will recognize it as a normal executable, one can simply step into the code
 add_executable(runtime ${CMAKE_CURRENT_BINARY_DIR}/runtime.4.o elf.c notify.c getsection.c)
+# CMake gets confused by the .o object, therefore we need to tell it that it shall link everything using the C compiler
 set_property(TARGET runtime PROPERTY LINKER_LANGUAGE C)
 target_link_libraries(runtime PRIVATE squashfuse dl xz libzlib pthread)
 
-add_custom_command(
-    TARGET runtime
-    POST_BUILD
-    COMMAND printf '\\x41\\x49\\x02' | dd of=runtime bs=1 seek=8 count=3 conv=notrunc
-)
+# embed the magic bytes after the runtime's build has finished
+if(APPIMAGEKIT_EMBED_MAGIC_BYTES)
+    add_custom_command(
+        TARGET runtime
+        POST_BUILD
+        COMMAND printf '\\x41\\x49\\x02' | dd of=runtime bs=1 seek=8 count=3 conv=notrunc
+    )
+endif()
 
+# required for embedding in appimagetool
 add_custom_command(
     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/runtime_embed.o
     COMMAND xxd -i runtime | ${CMAKE_C_COMPILER} -c -x c - -o runtime_embed.o
