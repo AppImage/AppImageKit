0c44a496a8d9d06e976d06d350fb5484ee7991aa
TheAssassin <theassassin@assassinate-you.net>
2018-07-17 02:44:54 +0200

    Improve toolchain

--- cmake/toolchains/i386-linux-gnu.cmake
+++ cmake/toolchains/i386-linux-gnu.cmake
@@ -2,6 +2,7 @@
 # - all dependencies that would be needed for a normal build must be installed as i386 versions
 # - building XZ/liblzma doesn't work yet, so one has to install liblzma-dev:i386 and set -DUSE_SYSTEM_XZ=ON
 # - building GTest doesn't work yet, so one has to install libgtest-dev:i386 and set -DUSE_SYSTEM_GTEST=ON
+# - building libarchive doesn't work yet, so one has to install liblzma-dev:i386 and set -DUSE_SYSTEM_LIBARCHIVE=ON (TODO: link system libarchive statically like liblzma)
 # some of the packets interfere with their x86_64 version (e.g., libfuse-dev:i386, libglib2-dev:i386), so building on a
 # normal system will most likely not work, but on systems like Travis it should work fine
 
@@ -18,3 +19,9 @@ set(CMAKE_EXE_LINKER_FLAGS "-m32" CACHE STRING "" FORCE)
 # build = build system
 # both must be specified
 set(EXTRA_CONFIGURE_FLAGS "--host=i686-pc-linux-gnu" "--build=x86_64-pc-linux-gnu" CACHE STRING "" FORCE)
+
+# may help with some rare issues
+set(CMAKE_PREFIX_PATH /usr/lib/i386-linux-gnu CACHE STRING "" FORCE)
+
+# makes sure that at least on Ubuntu pkg-config will search for the :i386 packages
+set(ENV{PKG_CONFIG_PATH} /usr/lib/i386-linux-gnu/pkgconfig/ CACHE STRING "" FORCE)
