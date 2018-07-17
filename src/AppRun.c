94393c271c684a92e4cbbb4b5f757837022dd65b
Jason C. McDonald <jcmcdonald@mousepawmedia.com>
2018-07-17 13:40:21 -0700

    Finished CSI commenting on AppRun.c

--- src/AppRun.c
+++ src/AppRun.c
@@ -135,6 +135,7 @@ int main(int argc, char *argv[]) {
     /* FIXME: This should use a while or do-while, instead of a for with an
      * arbitrary character range (LINE_SIZE = 255). This is both inefficient
      * and prone to undesired edge-case behavior.
+     * Also, why in the hey aren't we using the `exe` string?
      */
     for (n = 0; n < LINE_SIZE; n++) {
         // If we've reached the end of the string/line, break out of loop.
@@ -158,30 +159,52 @@ int main(int argc, char *argv[]) {
             // FIXME: This logic doesn't account for escaped quotes (\")
             in_quotes = !in_quotes;
         }
-        /* If the current character is a quote, change it to a NULL CHAR.
-         * NOTE: Why?
+        /* If the current character is a space, change it to a NULL CHAR,
+         * effectively splitting the string into multiple strings.
+         * This will allow us to iterate through the space-delimited args
+         * in the upcoming step.
          */
         else if (line[n] == ' ' && !in_quotes)
             line[n] = '\0';
     }
 
-    // count arguments
+    // Create a pointer to each "substring" within `line`.
     char*   arg         = exe;
+    // Counter for the number of arguments found.
     int     argcount    = 0;
+
+    /* FIXME: The following seems to only abort the loop on a NULL pointer!
+     * Possible undefined behavior if `line+1` is a valid (thus wild) pointer.
+     */
+    // Loop through each memory-adjacent string, counting them up.
     while ((arg += (strlen(arg)+1)) && *arg)
         argcount += 1;
 
-    // merge args
+    /* Merge the `Exec=` arguments with the `argv` arguments
+     * from the call to `main()`.
+     */
+    // Store the final list of arguments as an array of C-strings.
     char*   outargptrs[argcount + argc + 1];
+    // Store the name of the executable as the first argument.
     outargptrs[0] = exe;
+    // Keep track of which argument (index) we're storing.
     int     outargindex = 1;
+    /* Reset the `arg` pointer to the beginning of the sequence of
+     * memory-adjacent strings.
+     */
     arg                 = exe;
-    int     argc_       = argc - 1;     // argv[0] is the filename
+    // Skip the first argument, which is the filename, from the `main()` call.
+    int     argc_       = argc - 1;
     char**  argv_       = argv + 1;
+    // Iterate through each argument from `Exec()`.
+    // FIXME: Same possible undefined behavior as earlier.
     while ((arg += (strlen(arg)+1)) && *arg) {
-        if (arg[0] == '%' || (arg[0] == '"' && arg[1] == '%')) {         // handle desktop file field codes
+        // If the current argument is a desktop file field code...
+        if (arg[0] == '%' || (arg[0] == '"' && arg[1] == '%')) {
+            // Get the actual code after the `%` or `"%`
             char code = arg[arg[0] == '%' ? 1 : 2];
             switch(code) {
+                // For single file or URL args, merge in one of the `argv` args.
                 case 'f':
                 case 'u':
                     if (argc_ > 0) {
@@ -189,6 +212,9 @@ int main(int argc, char *argv[]) {
                         argc_--;
                     }
                     break;
+                /* For multiple file or URL args, merge in the rest of the
+                 * `argv` args
+                 */
                 case 'F':
                 case 'U':
                     while (argc_ > 0) {
@@ -196,34 +222,49 @@ int main(int argc, char *argv[]) {
                         argc_--;
                     }
                     break;
+                /* The Icon (i), translated name (c), and desktop file location
+                 * (k) keys are not supported. Throw a non-fatal error.
+                 */
                 case 'i':
                 case 'c':
                 case 'k':
                     fprintf(stderr, "WARNING: Desktop file field code %%%c is not currently supported\n", code);
                     break;
+                // Everything else is invalid. Throw a non-fatal error.
+                // TODO: Properly support deprecated codes (accept but remove) according to spec (???)
                 default:
                     fprintf(stderr, "WARNING: Invalid desktop file field code %%%c\n", code);
                     break;
             }
+
+        // If the current arg is anything else, just store it as-is.
         } else {
             outargptrs[outargindex++] = arg;
         }
     }
+
+    // Iterate through and store all of the remaining `argv` arguments.
     while (argc_ > 0) {
         outargptrs[outargindex++] = *argv_++;
         argc_--;
     }
-    outargptrs[outargindex] = '\0';     // trailing null argument required by execvp()
+    // A trailing null argument is required by `execvp()`
+    outargptrs[outargindex] = '\0';
 
-    // change directory
+    // Change directory to the application directory we found earlier.
+
+    // Calculate the length of the application directory path string.
     size_t appdir_s = strlen(appdir);
+    // Define the path to the ./usr subfolder in the application directory.
     char *usr_in_appdir = malloc(appdir_s + 5);
     snprintf(usr_in_appdir, appdir_s + 5, "%s/usr", appdir);
+    // Attempt to change to the `<appdir>/usr` directory.
     ret = chdir(usr_in_appdir);
+    // If we encounter a problem changing to that directory, throw a fatal error.
     if (ret != 0)
         die("Could not cd into %s\n", usr_in_appdir);
 
-    // set environment variables
+    // Append to the environment variables, so the AppImage can run correctly.
     char *old_env;
     size_t length;
     const char *format;
@@ -231,39 +272,55 @@ int main(int argc, char *argv[]) {
     /* https://docs.python.org/2/using/cmdline.html#envvar-PYTHONHOME */
     SET_NEW_ENV(new_pythonhome, appdir_s, "PYTHONHOME=%s/usr/", appdir);
 
+    // Append the system path.
     old_env = getenv("PATH") ?: "";
     SET_NEW_ENV(new_path, appdir_s*5 + strlen(old_env), "PATH=%s/usr/bin/:%s/usr/sbin/:%s/usr/games/:%s/bin/:%s/sbin/:%s", appdir, appdir, appdir, appdir, appdir, old_env);
 
+    // Append the system library path.
     old_env = getenv("LD_LIBRARY_PATH") ?: "";
     SET_NEW_ENV(new_ld_library_path, appdir_s*10 + strlen(old_env), "LD_LIBRARY_PATH=%s/usr/lib/:%s/usr/lib/i386-linux-gnu/:%s/usr/lib/x86_64-linux-gnu/:%s/usr/lib32/:%s/usr/lib64/:%s/lib/:%s/lib/i386-linux-gnu/:%s/lib/x86_64-linux-gnu/:%s/lib32/:%s/lib64/:%s", appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, old_env);
 
+    // Append the Python path.
     old_env = getenv("PYTHONPATH") ?: "";
     SET_NEW_ENV(new_pythonpath, appdir_s + strlen(old_env), "PYTHONPATH=%s/usr/share/pyshared/:%s", appdir, old_env);
 
+    // Append the XDG path
     old_env = getenv("XDG_DATA_DIRS") ?: "/usr/local/share/:/usr/share/";
     SET_NEW_ENV(new_xdg_data_dirs, appdir_s + strlen(old_env), "XDG_DATA_DIRS=%s/usr/share/:%s", appdir, old_env);
 
+    // Append the Perl path
     old_env = getenv("PERLLIB") ?: "";
     SET_NEW_ENV(new_perllib, appdir_s*2 + strlen(old_env), "PERLLIB=%s/usr/share/perl5/:%s/usr/lib/perl5/:%s", appdir, appdir, old_env);
 
+    // Append the GSettings Schema path
     /* http://askubuntu.com/questions/251712/how-can-i-install-a-gsettings-schema-without-root-privileges */
     old_env = getenv("GSETTINGS_SCHEMA_DIR") ?: "";
     SET_NEW_ENV(new_gsettings_schema_dir, appdir_s + strlen(old_env), "GSETTINGS_SCHEMA_DIR=%s/usr/share/glib-2.0/schemas/:%s", appdir, old_env);
 
+    //Append the Qt Path
     old_env = getenv("QT_PLUGIN_PATH") ?: "";
     SET_NEW_ENV(new_qt_plugin_path, appdir_s*10 + strlen(old_env), "QT_PLUGIN_PATH=%s/usr/lib/qt4/plugins/:%s/usr/lib/i386-linux-gnu/qt4/plugins/:%s/usr/lib/x86_64-linux-gnu/qt4/plugins/:%s/usr/lib32/qt4/plugins/:%s/usr/lib64/qt4/plugins/:%s/usr/lib/qt5/plugins/:%s/usr/lib/i386-linux-gnu/qt5/plugins/:%s/usr/lib/x86_64-linux-gnu/qt5/plugins/:%s/usr/lib32/qt5/plugins/:%s/usr/lib64/qt5/plugins/:%s", appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, appdir, old_env);
 
-    /* Otherwise may get errors because Python cannot write __pycache__ bytecode cache */
+    /* We need this, otherwise we may get errors because Python cannot
+     * write __pycache__ bytecode cache.
+     */
     putenv("PYTHONDONTWRITEBYTECODE=1");
 
-    /* Run */
+    /* Execute the program, using the executable name from the .desktop
+     * file (`exe`) and the final merged array of arguments.
+     */
     ret = execvp(exe, outargptrs);
 
+    // Store the last error number; in this case, the one from `execvp(...)`.
     int error = errno;
 
+    /* If `execvp(...)` returned a negative value, throw a fatal error using
+     * the retrieved error code.
+     */
     if (ret == -1)
         die("Error executing '%s': %s\n", exe, strerror(error));
 
+    // Deallocate everything we've malloc'd.
     free(line);
     free(desktop_file);
     free(usr_in_appdir);
@@ -275,5 +332,6 @@ int main(int argc, char *argv[]) {
     free(new_perllib);
     free(new_gsettings_schema_dir);
     free(new_qt_plugin_path);
+
     return 0;
 }
