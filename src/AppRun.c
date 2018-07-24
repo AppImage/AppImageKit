a6a12dce35728ac783b199c5f658af8a00eddd29
Jason C. McDonald <jcmcdonald@mousepawmedia.com>
2018-07-23 22:05:10 -0700

    Refactoring (the unsaved changes from before. Oops!)

--- src/AppRun.c
+++ src/AppRun.c
@@ -25,62 +25,56 @@ THE SOFTWARE.
 
 **************************************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <libgen.h>
 #include <dirent.h>
-#include <string.h>
-#include <errno.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-/** Macro to throw error and exit. */
-#define die(...)                                    \
-    do {                                            \
-        fprintf(stderr, "Error: " __VA_ARGS__);     \
-        exit(1);                                    \
-    } while(0);
-
-/** Macro to set environmental variables. */
-#define SET_NEW_ENV(str,len,fmt,...)                \
-    format = fmt;                                   \
-    length = strlen(format) + (len);                \
-    char *str = calloc(length, sizeof(char));     \
-    snprintf(str, length, format, __VA_ARGS__);   \
+/** Macro to set environment variables. */
+#define SET_NEW_ENV(str, len, fmt, ...)         \
+    format = fmt;                               \
+    length = strlen(format);                    \
+    char* str = calloc(length, sizeof(char));   \
+    snprintf(str, length, format, __VA_ARGS__);  \
     putenv(str);
 
 /** Macro to return the largest of a pair of numbers */
-#define MAX(a,b)    (a > b ? a : b)
+#define MAX(a, b) (a > b? a : b)
 
-/* Define the boolean type and its constants to use integers, with n<0 == true.
- * FIXME: This is terrible, bad and wrong. Fix it in refactor.
- */
-#define bool int
-#define false 0
-#define true -1
-
+// TODO: Is this arbitrary? Check and refine.
 #define LINE_SIZE 255
 
-/** Check for a *.desktop file at the given path.
-  * \param the file path to check
-  * \return True (1) if the path points to a .desktop file, else false (0).
-  */
-int filter(const struct dirent *dir) {
-    // Get pointer to file path.
-    char *p = (char*) &dir->d_name;
-    // Read only the file extension (start reading from last dot in path)
-    p = strrchr(p, '.');
-    // Return true if the file extension .desktop is found
-    return p && !strcmp(p, ".desktop");
+/** Throw error and exit. */
+void die(const char * format, ...) {
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+    exit(1);
 }
+
+/** Check for a .desktop file at the given path.
+  * \param the file path to check
+  * \return true if the path points to a .desktop file, else false.
+  */
+ int filter(const struct dirent* dir) {
+     // Get pointer to file path.
+     char* p = (char*) &dir->d_name;
+     // Read only the file extension (start reading from last dot in path)
+     p = strrchr(p, '.');
+     // Return true if the file extension .desktop is found.
+     return (p && !strcmp(p, ".desktop")) ? 1 : 0;
+ }
 
 int main(int argc, char *argv[]) {
     // Get path to the application directory.
-    char *appdir = dirname(realpath("/proc/self/exe", NULL));
+    char* appdir = dirname(realpath("/proc/self/exe", NULL));
     /* If there's a problem accessing the application directory,
      * throw fatal error.
      */
-    if (!appdir)
+    if (!appdir) {
         die("Could not access /proc/self/exe\n");
+    }
 
     // Check for .desktop files
     // The return code for whether we found a .desktop file
@@ -90,118 +84,109 @@ int main(int argc, char *argv[]) {
     // Scan for .desktop files.
     ret = scandir(appdir, &namelist, filter, NULL);
 
-    // If we found no .desktop files, there's nothing to run. Fatal error.
     if (ret == 0) {
-        die("No .desktop files found\n");
-
-    // If there was an error with scandir(...), we can't go on. Fatal error.
+        // If we found no .desktop files, there's nothing to run. Fatal error.
+        die("No .desktop files found.\n");
     } else if(ret == -1) {
+        // If there was an error with scandir(), we can't go on. Fatal error.
         die("Could not scan directory %s\n", appdir);
     }
 
-    /* Extract executable from .desktop file */
-    char *desktop_file = calloc(LINE_SIZE, sizeof(char));
+    /* Extract executable from the first (and presumably only) desktop
+     * file found in the application directory.
+     */
+    char* desktop_file = calloc(LINE_SIZE, sizeof(char));
     snprintf(desktop_file, LINE_SIZE, "%s/%s", appdir, namelist[0]->d_name);
-    FILE *f     = fopen(desktop_file, "r");
+    // Open the desktop file for reading.
+    FILE* f = fopen(desktop_file, "r";)
     // Store the line we're reading.
-    char *line  = malloc(LINE_SIZE);
+    char* line = malloc(LINE_SIZE);
     // Skip the `Exec=` prefix on `line`, only looking at the stored value.
-    char *exe   = line+5;
-    size_t n    = LINE_SIZE;
+    char* exe = line + 5;
+    size_t n = LINE_SIZE;
 
-    /* Search each line of our file for the 'Exec=' entry.
+    /* Search each line of our file for the `Exec=` entry.
      * To search, we store the line in `line`, and then check for the
      * correct prefix. If it doesn't match, we keep looking.
      */
     do {
-        // If we reach the end of the file without finding 'Exec=', fatal error.
-        if (getline(&line, &n, f) == -1)
-            die("Executable not found, make sure there is a line starting with 'Exec='\n");
-    } while(strncmp(line, "Exec=", 5));
+        // If we reach the end of the file without finding `Exec=`, fatal error.
+        if (getline(&line, &n, f) == -1) {
+            die("Executable not found. Make sure there is a line starting with 'Exec='\n");
+        }
+    } while(strncmp(line, "Exec", 5));
     // We now have the line we need, close the file.
     fclose(f);
 
-    /* FIXME: We aren't ensuring that 'exe' has a value!
-     * If the entire line is `Exec=`, attempting to access `exe` will result
-     * in undefined behavior (wild pointer).
+    /* If the line we found has nothing more than `Exec=`, it's missing a value.
+     * This means the .desktop file is malformed. Fatal error.
      */
-
-    /* Parse arguments */
-
-    // Flag if we're within quotation marks.
-    bool in_quotes = 0;
-
-    // Loop through each character in a line.
-    /* FIXME: This should use a while or do-while, instead of a for with an
-     * arbitrary character range (LINE_SIZE = 255). This is both inefficient
-     * and prone to undesired edge-case behavior.
-     * Also, why in the hey aren't we using the `exe` string?
-     */
-    for (n = 0; n < LINE_SIZE; n++) {
-        // If we've reached the end of the string/line, break out of loop.
-        if (!line[n]) {
-            break;
-
-        /* If we encounter a LINE FEED (10) or CARRIAGE RETURN (13)... */
-        } else if (line[n] == 10 || line[n] == 13) {
-            /* Change the next three characters to NULL CHAR.
-             * NOTE: Why?
-             * FIXME: What if [n+1] and/or [n+2] are past the end of the string?
-             */
-            line[n] = '\0';
-            line[n+1] = '\0';
-            line[n+2] = '\0';
-            // Break out of loop.
-            break;
-
-        // If the current character is a quote, invert our `in_quotes` flag.
-        } else if (line[n] == '"') {
-            // FIXME: This logic doesn't account for escaped quotes (\")
-            in_quotes = !in_quotes;
-        }
-        /* If the current character is a space, change it to a NULL CHAR,
-         * effectively splitting the string into multiple strings.
-         * This will allow us to iterate through the space-delimited args
-         * in the upcoming step.
-         */
-        else if (line[n] == ' ' && !in_quotes)
-            line[n] = '\0';
+    if( strlen(line) <= 5) {
+        die("Executable not found. Make sure 'Exec=' has a value.\n");
     }
 
-    // Create a pointer to each "substring" within `line`.
-    char*   arg         = exe;
-    // Counter for the number of arguments found.
-    int     argcount    = 0;
+    // Flag if we're within quotation marks.
+    bool in_quotes = false;
 
-    /* FIXME: The following seems to only abort the loop on a NULL pointer!
-     * Possible undefined behavior if `line+1` is a valid (thus wild) pointer.
-     */
+    // Loop through each character in a line.
+    int n = 0;
+    do {
+        if ((exe[n] == 10 || exe[n] == 13) && exe[n+1] && exe[n+2]) {
+            /* If we encounter a LINE FEED (10) or CARRIAGE RETURN (13), change
+            * the next three characters to NULL CHAR. This acts as a sentry
+            * while parsing through all the substrings later.
+            * (We also confirm that the next two characters aren't already null.)
+            */
+            exe[n] = '\0';
+            exe[n+1] = '\0';
+            exe[n+2] = '\0';
+        } else if (exe[n] == '"' && exe[n-1] != '\\') {
+            /* If the current character is a non-escaped quote, toggle whether
+             * we are parsing inside a string.
+             */
+            in_quotes = !in_quotes;
+        } else if (line[n] == ' ' && !in_quotes) {
+            /* If the current character is a space, change it to a NULL CHAR,
+            * effectively splitting the string into multiple strings.
+            * This will allow us to iterate through the space-delimited args
+            * in the upcoming step.
+            */
+            exe[n] = '\0';
+        }
+    } while (exe[++n] != '\0');
+
+    // Create a pointer to each "substring" within `line`.
+    char* arg = exe;
+    // Counter for the number of arguments found.
+    int argcount = 0;
+
     // Loop through each memory-adjacent string, counting them up.
-    while ((arg += (strlen(arg)+1)) && *arg)
+    while ((arg += (strlen(arg)+1)) && *arg != '\0') {
         argcount += 1;
+    }
 
     /* Merge the `Exec=` arguments with the `argv` arguments
      * from the call to `main()`.
      */
+
     // Store the final list of arguments as an array of C-strings.
-    char*   outargptrs[argcount + argc + 1];
+    char* outargptrs[argcount + argc + 1];
     // Store the name of the executable as the first argument.
     outargptrs[0] = exe;
     // Keep track of which argument (index) we're storing.
-    int     outargindex = 1;
+    int outargindex = 1;
     /* Reset the `arg` pointer to the beginning of the sequence of
      * memory-adjacent strings.
      */
-    arg                 = exe;
+    arg = exe;
     // Skip the first argument, which is the filename, from the `main()` call.
-    int     argc_       = argc - 1;
-    char**  argv_       = argv + 1;
-    // Iterate through each argument from `Exec()`.
-    // FIXME: Same possible undefined behavior as earlier.
+    int argc_ = argc - 1;
+    char** argv_ = argv + 1;
+    // Iterate through each argument from `Exec=`.
     while ((arg += (strlen(arg)+1)) && *arg) {
-        // If the current argument is a desktop file field code...
+        // If the current argument is a desktop file field code.
         if (arg[0] == '%' || (arg[0] == '"' && arg[1] == '%')) {
-            // Get the actual code after the `%` or `"%`
+            // Get the actual code after the `%` or `"5`.
             char code = arg[arg[0] == '%' ? 1 : 2];
             switch(code) {
                 // For single file or URL args, merge in one of the `argv` args.
@@ -209,46 +194,48 @@ int main(int argc, char *argv[]) {
                 case 'u':
                     if (argc_ > 0) {
                         outargptrs[outargindex++] = *argv_++;
-                        argc_--;
+                        --argc_;
                     }
                     break;
                 /* For multiple file or URL args, merge in the rest of the
-                 * `argv` args
+                 * `argv` args.
                  */
                 case 'F':
                 case 'U':
                     while (argc_ > 0) {
                         outargptrs[outargindex++] = *argv_++;
-                        argc_--;
+                        --argc_;
                     }
                     break;
                 /* The Icon (i), translated name (c), and desktop file location
-                 * (k) keys are not supported. Throw a non-fatal error.
-                 */
+                * (k) keys are not supported. Throw a non-fatal error.
+                */
                 case 'i':
                 case 'c':
                 case 'k':
-                    fprintf(stderr, "WARNING: Desktop file field code %%%c is not currently supported\n", code);
+                    fprintf(stderr, "WARNING: Desktop field field %%%c is not currently supported.\n", code);
                     break;
                 // Everything else is invalid. Throw a non-fatal error.
-                // TODO: Properly support deprecated codes (accept but remove) according to spec (???)
+                /* TODO: Properly support deprecated codes (accept but remove)
+                 * according to spec (???)
+                 */
                 default:
-                    fprintf(stderr, "WARNING: Invalid desktop file field code %%%c\n", code);
+                    fprintf(stderr, "WARNING: Invalid desktop file field code %%%c.\n", code);
                     break;
             }
-
-        // If the current arg is anything else, just store it as-is.
         } else {
+            // If the current arg is anything else, just store it as-is.
             outargptrs[outargindex++] = arg;
         }
     }
 
-    // Iterate through and store all of the remaining `argv` arguments.
+    // Iterate through and store all of the remaining 'argv' arguments.
     while (argc_ > 0) {
         outargptrs[outargindex++] = *argv_++;
-        argc_--;
+        --argc_;
     }
-    // A trailing null argument is required by `execvp()`
+
+    // A trailing null argument is required by `execvp()`.
     outargptrs[outargindex] = '\0';
 
     // Change directory to the application directory we found earlier.
@@ -257,12 +244,13 @@ int main(int argc, char *argv[]) {
     size_t appdir_s = strlen(appdir);
     // Define the path to the ./usr subfolder in the application directory.
     char *usr_in_appdir = malloc(appdir_s + 5);
-    snprintf(usr_in_appdir, appdir_s + 5, "%s/usr", appdir);
+    snprintf(usr_in_appdir, appdir_s + 1, "%s/usr", appdir);
     // Attempt to change to the `<appdir>/usr` directory.
     ret = chdir(usr_in_appdir);
     // If we encounter a problem changing to that directory, throw a fatal error.
-    if (ret != 0)
+    if (ret != 0) {
         die("Could not cd into %s\n", usr_in_appdir);
+    }
 
     // Append to the environment variables, so the AppImage can run correctly.
     char *old_env;
@@ -313,16 +301,17 @@ int main(int argc, char *argv[]) {
      */
     ret = execvp(exe, outargptrs);
 
-    // Store the last error number; in this case, the one from `execvp(...)`.
+    // Store the last error number; in this case, the one from `execvp()`.
     int error = errno;
 
-    /* If `execvp(...)` returned a negative value, throw a fatal error using
+    /* If `execvp()` returned a negative value, throw a fatal error using
      * the retrieved error code.
      */
-    if (ret == -1)
+    if (ret == -1) {
         die("Error executing '%s': %s\n", exe, strerror(error));
+    }
 
-    // Deallocate everything we've malloc'd.
+    // Deallocated everything we've malloc'd.
     free(line);
     free(desktop_file);
     free(usr_in_appdir);
