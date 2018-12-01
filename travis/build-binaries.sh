e409754848b2b6190594a420c7eed0d4e4fa06d4
TheAssassin <theassassin@users.noreply.github.com>
2018-12-01 02:32:56 +0100

    Make sure the prebuilt libraries in the container will be found

--- travis/build-binaries.sh
+++ travis/build-binaries.sh
@@ -7,4 +7,7 @@ cd /AppImageKit
 
 ./build.sh "$@"
 
+# make sure the prebuilt libraries in the container will be found
+export LD_LIBRARY_PATH=/deps/lib:"$LD_LIBRARY_PATH"
+
 ./test-appimagetool.sh build/install_prefix/usr/bin/appimagetool
