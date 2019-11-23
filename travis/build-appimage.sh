64321b72bc35fc886772ab7d95f3924b67207b32
TheAssassin <theassassin@users.noreply.github.com>
2019-11-23 22:50:18 +0100

    Actually sign AppImages, mk. 2
    Travis documents that the variable should be set to "false", but we just
    tested and the variable is empty/not set outside PR builds. Therefore we
    had to extend the check.

--- travis/build-appimage.sh
+++ travis/build-appimage.sh
@@ -6,7 +6,7 @@ cd /AppImageKit/build
 
 cd out/
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] || [ "$TRAVIS_PULL_REQUEST" == "" ]; then
     # Sign only when NOT on a pull request
     ./appimagetool.AppDir/AppRun ./appimagetool.AppDir/ -s -v \
         -u "gh-releases-zsync|AppImage|AppImageKit|continuous|appimagetool-$ARCH.AppImage.zsync" \
